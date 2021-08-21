b0db5227a28a1c7ab310776666f2642c993eeb71
KyliannBr <brisson.kyliann12122001@gmail.com>
2021-08-21 23:30:05 +0200

    init

---
+++ php/mysqlinfo.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+$servername = "sql109.byethost3.com";
+$username = "b3_27764987";
+$password = "brisson1212";
+$myDB="b3_27764987_locationvoiture";
+*/
+$servername = "localhost";
+$usernameDB = "kyliann";
+$passwordDB = "brisson";
+$myDB="partage";
+
+?>
